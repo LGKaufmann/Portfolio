1f98cc1173028c88787388cedf5f51acd34c0478
LGKaufmann <lautykaufmann76@gmail.com>
2023-11-21 21:56:06 -0300

    ultimando cambios

--- app/Components/AboutSection.jsx
+++ app/Components/AboutSection.jsx
@@ -51,12 +51,12 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:p">
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/about-image.png" width={800} height={600} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">Sobre Mi</h2>
-          <p className="text-sm text-align: justify md:text-lg text-align: justify">
+          <p className="text-sm  lg:text-lg">
             ¡Hola! Soy Lautaro Kaufmann, un apasionado desarrollador full stack
             de Argentina Con solo 22 años de edad. Mi viaje en el desarrollo
             comenzó con un amor por el diseño web y una curiosidad insaciable
--- app/Components/TabButton.jsx
+++ app/Components/TabButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const variants = {
   default: { width: 0 },
@@ -6,15 +7,18 @@ const variants = {
 };
 
 const TabButton = ({ active, selectTab, children }) => {
-  const buttonClasses = active
-    ? "text-white border-b-2 border-blue-600"
-    : "text-[#ADB7BE]";
+  const buttonClasses = active ? "text-white" : "text-[#ADB7BE]";
 
   return (
     <button onClick={selectTab}>
       <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
         {children}
       </p>
+      <motion.div
+        animate={active ? "active" : "default"}
+        variants={variants}
+        className="h-1 bg-primary-500 mt-2 mr-3"
+      ></motion.div>
     </button>
   );
 };
